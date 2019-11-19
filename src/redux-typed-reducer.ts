49a465823766fc3a2457eb147177e97e11dc000b
Emilio Platzer <emilioplatzer@gmail.com>
2019-11-18 23:39:14 -0300

    saco el TPayload de la definición del tipo para poder deducirla del caso.

--- src/redux-typed-reducer.ts
+++ src/redux-typed-reducer.ts
@@ -1,33 +1,43 @@
 import * as likeAr from "like-ar";
 
-type ReturnsTheSameThatReceives<T> = {
-    [K in keyof T]: T[K] extends (param:infer U) => any ? (param:U) => U & {type:K} : never
+export type ReturnsTheSameThatReceives<T> = {
+    [K in keyof T]: T[K] extends (param:infer U) => any ? (param:U) => U & {type:K} : {type:K}
 }
 
-type Reducer<TState, TPayload> = (payload:TPayload) => ((state:TState) => TState);
+export type Reducer<TState, TPayload> = (payload:TPayload) => ((state:TState) => TState);
 
-type Reducers<TState, TPayload, TActionNames extends string> = {
-    [key in TActionNames] : Reducer<TState, TPayload>
+export type Reducers<TState, T> = {
+    [TActionNames in keyof T] : Reducer<TState, T[TActionNames]>
 }
 
-type ActionsFrom<TPayload, TActionNames extends string> = {type:TActionNames, payload:TPayload};
+var dispatchOne = function <TState, T, TReducers extends Reducers<TState, T>>(reducers:TReducers, type:keyof TReducers){
+    const dispatchers = createDispatchers<TState, T, TReducers>(reducers);
+    const result = dispatchers[type];
+    return result;
+}
 
-export function createDispatchers<TState, TPayload, TActionNames extends string, T extends Reducers<TState, TPayload, TActionNames>>(actions:T){
-    const dispatcher:ReturnsTheSameThatReceives<T> = 
+export type ActionsFrom2<TState, T, TReducers extends Reducers<TState, T>, TActionName extends keyof TReducers > = TReducers[TActionName];
+
+export type ActionsFrom<TState, T, TReducers extends Reducers<TState, T>> = ReturnsTheSameThatReceives<TReducers>[keyof TReducers];
+
+//    ReturnType<typeof dispatchOne<TState, T, TReducers>() >;
+
+export function createDispatchers<TState, T, TReducers extends Reducers<TState, T>>(reducers:TReducers){
+    const dispatcher:ReturnsTheSameThatReceives<TReducers> = 
         // @ts-ignore hay un problema con likeAr.map
-        likeAr<T>(actions).map(function(_:Reducer<TState, TPayload>, name:string){
-            return (payload:TPayload)=>({type:name, payload: payload});
+        likeAr<TReducers>(reducers).map(function(_:Reducer<TState, T>, name:string){
+            // @ts-ignore hay un problema con likeAr.map
+            return (payload)=>({type:name, payload: payload});
         }).plain();
     return dispatcher;
 }
 
-export function createReducer<TState, TPayload, TActionNames extends string>(reducers:Reducers<TState, TPayload, TActionNames>, initialState:TState){
-    return function reducerFunction(state:TState = initialState, action: ActionsFrom<TPayload, TActionNames>){
+export function createReducer<TState, T, TReducers extends Reducers<TState, T>>(reducers:TReducers, initialState:TState){
+    return function reducerFunction(state:TState = initialState, action: ActionsFrom<TState, T, TReducers>){
         if(action.type in reducers){
             return reducers[action.type](action.payload)(state);
         }
         return {...state};
     }
 }
-
 
